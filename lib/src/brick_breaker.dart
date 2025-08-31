43c66c5645d92f4f77041baf8f5c082b590aef91
jydv402 <jydv402@gmail.com>
2025-08-31 23:25:20 +0530

    Added ball

--- lib/src/brick_breaker.dart
+++ lib/src/brick_breaker.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math' as math;
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
@@ -10,11 +11,13 @@ class BrickBreaker extends FlameGame {
   BrickBreaker()
     : super(
         camera: CameraComponent.withFixedResolution(
+          // Form config.dart
           width: gameWidth,
           height: gameHeight,
         ),
       );
 
+  final rand = math.Random();
   double get width => size.x;
   double get height => size.y;
 
@@ -25,5 +28,18 @@ class BrickBreaker extends FlameGame {
     camera.viewfinder.anchor = Anchor.topLeft;
 
     world.add(PlayArea());
+
+    world.add(
+      Ball(
+        velocity: Vector2(
+          (rand.nextDouble() - 0.5) * width,
+          height * 0.2,
+        ).normalized()..scale(height / 4),
+        position: size / 2,
+        radius: ballRadius,
+      ),
+    );
+
+    debugMode = true;
   }
 }
